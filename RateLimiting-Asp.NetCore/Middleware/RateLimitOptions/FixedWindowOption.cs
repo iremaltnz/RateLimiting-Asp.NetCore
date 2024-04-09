def9c603e0c84d88eefcb18af69e4acd6b8cbeef
iremaltnz <iremaltnz@gmail.com>
2024-04-10 02:30:13 +0300

    Fixed Window Limit

---
+++ RateLimiting-Asp.NetCore/Middleware/RateLimitOptions/FixedWindowOption.cs
@@ -0,0 +1,11 @@
+﻿namespace RateLimiting_Asp.NetCore.Middleware.RateLimitOptions
+{
+    public class FixedWindowOption
+    {
+        public const string MyRateLimit = "MyRateLimit";
+        public int PermitLimit { get; set; } = 2;
+        public int Window { get; set; } = 15;
+        public int QueueLimit { get; set; } = 3;
+        public bool AutoReplenishment { get; set; } = false;
+    }
+}
--- RateLimiting-Asp.NetCore/Program.cs
+++ RateLimiting-Asp.NetCore/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.Extensions.Options;
+using NuGet.Common;
+using RateLimiting_Asp.NetCore.Middleware.RateLimitOptions;
+using System.Threading.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +13,28 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var myOptions = new FixedWindowOption();
+builder.Configuration.GetSection(FixedWindowOption.MyRateLimit).Bind(myOptions);
+var fixedPolicy = "fixed";
+
+builder.Services.AddRateLimiter(_ =>
+{
+    _.AddFixedWindowLimiter(policyName: fixedPolicy, options =>
+    {
+        options.PermitLimit = myOptions.PermitLimit;
+        options.Window = TimeSpan.FromSeconds(myOptions.Window);
+        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        //options.QueueLimit = myOptions.QueueLimit;
+    });
+
+    _.OnRejected = async (context, token) =>
+    {      
+        context.HttpContext.Response.StatusCode = 429;
+        await context.HttpContext.Response.WriteAsync($"Too many requests. Please try later again...", cancellationToken: token);
+
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +43,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseRateLimiter();
+
+app.MapGet("/FixedWindow", () => Results.Ok($"Hello"))
+                           .RequireRateLimiting("fixed");
 
 app.UseHttpsRedirection();
 
